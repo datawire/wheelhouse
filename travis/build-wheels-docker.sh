730dbec11d3fc0685d40a9522633603e8d6afcfb
Rafael Schloming <rhs@alum.mit.edu>
2017-08-17 01:15:06 -0400

    dont clobber prebuild manylinux wheels

--- travis/build-wheels-docker.sh
+++ travis/build-wheels-docker.sh
@@ -12,7 +12,7 @@ for PYBIN in $(ls -d /opt/python/*/bin | fgrep cp27); do
 done
 
 # Bundle external shared libraries into the wheels
-for whl in $(ls wheelhouse/*.whl | fgrep -v none-any.whl); do
+for whl in $(ls wheelhouse/*.whl | fgrep -v none-any.whl | fgrep -v manylinux1_); do
     auditwheel repair "$whl" -w wheelhouse
     rm "$whl"
 done
