6678d7aea2f421c3b1c9edaef550eaff560844e1
Rafael Schloming <rhs@alum.mit.edu>
2018-01-18 14:24:14 -0500

    travis sucks

--- travis/build-wheels-osx.sh
+++ travis/build-wheels-osx.sh
@@ -2,7 +2,12 @@
 set -e -x
 
 # Compile wheels
-travis/build-wheels.sh pip python
+
+for PYV in 2.7.14 3.3.7 3.4.7 3.5.4 3.6.5; do
+    pyenv install ${PYV}
+    pyenv system ${PYV}
+    travis/build-wheels.sh pip python
+done
 
 # We build the generic stuff on linux
 rm wheelhouse/*-none-any.whl
