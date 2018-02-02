d5c9ebb90c06611da0ed83974e79a863ea6069dc
Rafael Schloming <rhs@alum.mit.edu>
2018-02-02 15:28:13 -0500

    try to fix for osx

--- travis/build-wheels-osx.sh
+++ travis/build-wheels-osx.sh
@@ -3,9 +3,10 @@ set -e -x
 
 # Compile wheels
 
-for PYV in 2.7.14 3.3.7 3.4.7 3.5.4 3.6.5; do
+for PYV in 2.7.14 3.3.7 3.4.7 3.5.4 3.6.4; do
     pyenv install ${PYV}
-    pyenv system ${PYV}
+    pyenv global ${PYV}
+    pip install wheel
     travis/build-wheels.sh pip python
 done
 
